36a2972208c36caa30e5e6efccbdcf9a0c7c8f6a
Adrián Rangel Suárez <arangelsuarez@hotmail.com>
2025-01-26 13:50:56 -0600

    Fistbump

--- Assets/Scripts/HandScript.cs
+++ Assets/Scripts/HandScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Collections;
 public class HandScript : MonoBehaviour
 {
     [SerializeField]
@@ -9,14 +10,20 @@ public class HandScript : MonoBehaviour
     private Sprite[] m_SpriteArray;
     [SerializeField]
     private PlayerInputManager playerImput;
+    [SerializeField]
+    private Animator otherHandAnimator;
+
+    private Animator myAnimator;
 
     private bool m_flagBottonPress = false;
     private int m_IndexSprite = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Image.sprite = m_SpriteArray[m_IndexSprite];
 
+        myAnimator = GetComponent<Animator>();
     }
 
     public void openHandShake(InputAction.CallbackContext ctx)
@@ -35,9 +42,32 @@ public class HandScript : MonoBehaviour
 
     private void changeHand()
     {
+        myAnimator.SetBool("IsFist", m_IndexSprite == 0);
+
         m_IndexSprite = (m_IndexSprite == 0) ? 1 : 0;
         m_Image.sprite = m_SpriteArray[m_IndexSprite];
     }
 
+    public void End()
+    {
+        if (otherHandAnimator.GetBool("IsFist") && myAnimator.GetBool("IsFist")) {
+            myAnimator.SetBool("Match", true);
+        } else if (!otherHandAnimator.GetBool("IsFist") && myAnimator.GetBool("IsFist")) {
+            myAnimator.SetBool("Match", false);
+        } else if (otherHandAnimator.GetBool("IsFist") && !myAnimator.GetBool("IsFist")) {
+            myAnimator.SetBool("Match", false);
+        } else {
+            myAnimator.SetBool("Match", true);
+        }
 
+        myAnimator.SetBool("Ended", true);
+
+        StartCoroutine(ResetBool());
+    }
+
+    private IEnumerator ResetBool()
+    {
+        yield return null;
+        myAnimator.SetBool("Ended", false);
+    }
 }
